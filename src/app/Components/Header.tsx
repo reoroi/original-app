ce5972bc5cbcc88f1c8515255f76ec8cd80875ed
reoroi <reo0330210@gmail.com>
2025-02-01 19:01:47 +0900

    サイドバーの修正

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -7,46 +7,51 @@ import CreateIcon from "@mui/icons-material/Create";
 import HomeIcon from "@mui/icons-material/Home";
 import { Button } from "@mui/material";
 import { currentUserContext } from "../useAuth";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { handleLogout } from "../Function/function";
 
 const Header = () => {
   const router = useRouter();
   const [isOpen, setIsOpen] = useState(false);
   const currentUser = useContext(currentUserContext);
-  console.log(currentUser,"ユーザ情報")
   return (
     <div className="flex w- ">
       <Button className="w-full" onClick={() => router.push("/Calendar")}>
-        <CalendarMonthIcon fontSize="large"/>
+        <CalendarMonthIcon fontSize="large" />
       </Button>
       <Button className="w-full" onClick={() => router.push("DiaryHome")}>
         <HomeIcon fontSize="large" />
-      </Button> 
+      </Button>
       <Button className="w-full" onClick={() => router.push("/AddSchedule")}>
-        <CreateIcon fontSize="large"/>
+        <CreateIcon fontSize="large" />
       </Button>
       <Button className="w-full" onClick={() => router.push("/DiarySearch")}>
-        <SearchIcon fontSize="large"/>
+        <SearchIcon fontSize="large" />
       </Button>
-      <Button className="w-full" onClick={() => setIsOpen(!isOpen)}><AccountCircleIcon fontSize="large"></AccountCircleIcon></Button>
+      <Button className="w-full" onClick={() => setIsOpen(!isOpen)}>
+        <AccountCircleIcon fontSize="large"></AccountCircleIcon>
+      </Button>
       {isOpen && (
         <div
-          className="fixed inset-0 bg-black bg-opacity-50 transition-opacity duration-300 "
+          className="fixed inset-0 bg-black bg-opacity-50 transition-opacity duration-300 z-[2]"
           onClick={() => setIsOpen(!isOpen)}
         />
       )}
 
       <nav
-        className={`block fixed bg-white top-0 rounded-s-2xl ${
-          isOpen ? "right-0" : "right-[-400px]"
-        }  bottom-0 transition-all  z-0 `}
+        className={`block fixed bg-white top-0 rounded-s-2xl bottom-0 transition-all z-[2]
+          ${isOpen ? "right-0" : "right-[-400px]"} `}
       >
-        <div className="p-5 text-2xl ">
+        <div className="flex flex-col p-5 text-2xl h-full ">
+          <p className="font-bold">{currentUser?.userName}</p>
           <p>{currentUser?.email}</p>
-          <p>{currentUser?.userName}</p>
-          <p>メール情報</p>
-          <p>可能であればパスワードの初期化</p>
-          <button className="bg-red-500 p-2 rounded-md ">ログアウト</button>
+          <button
+            onClick={handleLogout}
+            className="bg-red-500 rounded-md mt-auto p-2 border-b-red-700 solid border-b-4 active:bg-red-700 active:border-none hover:bg-red-600"
+          >
+            ログアウト
+          </button>
+          {/*  */}
         </div>
       </nav>
     </div>

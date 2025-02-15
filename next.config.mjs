dbd30f3bc7ac29f2d3c2bb87690b51b5c991eced
reoroi <reo0330210@gmail.com>
2025-02-16 03:54:13 +0900

    外部URLの表示の設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['knuiaygxohofaqvhhjye.supabase.co'],
+  }
+};
 
 export default nextConfig;
